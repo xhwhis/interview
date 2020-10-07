4482b608633d400de96bd2d806bb54e86b06506a
lws597 <lwscode@163.com>
2020-10-07 17:36:59 +0800

    2020/10/07

--- design_patterns/singleton_safe.cpp
+++ design_patterns/singleton_safe.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class Singleton {
 private:
-    static Singleton * pinstance_;
+    static Singleton *pinstance_;
     static mutex mutex_;
 
 protected:
@@ -16,7 +16,7 @@ protected:
 
 public:
     Singleton(Singleton &other) = delete;
-    void operator=(const Singleton &) = delete;
+    void operator=(const Singleton&) = delete;
     
     static Singleton *GetInstance(const string& value);
     void SomeBusinessLogic() {}
